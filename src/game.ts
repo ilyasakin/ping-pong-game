f362084108f6ba415d5d82a925084d679c8227b9
İlyas Akın <ashnwor@gmail.com>
2022-12-30 01:30:28 +0300

    Add Game class properties

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,18 @@
-class Game {}
+class Game {
+  private ctx: CanvasRenderingContext2D;
+
+  constructor(private readonly canvas: HTMLCanvasElement) {
+    this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+  }
+
+  private loop(): void {}
+
+  private init(): void {}
+
+  public start(): void {
+    this.init();
+    this.loop();
+  }
+}
 
 export default Game;
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import Game from './game';
 import './style.css';
 
-new Game();
+const canvas = document.getElementById(
+  'game-canvas'
+) as HTMLCanvasElement | null;
+
+if (!canvas) {
+  throw new Error('No canvas element found');
+}
+
+const game: Game = new Game(canvas);
+game.start();
