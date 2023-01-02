d1ec3797fbac79fa814cfe68cc6e8c2284ff428c
İlyas Akın <ashnwor@gmail.com>
2023-01-02 03:13:04 +0300

    Add update method on GameObject

--- src/game-object.ts
+++ src/game-object.ts
@@ -1,3 +1,12 @@
-class GameObject {}
+abstract class GameObject {
+  /**
+   * The update method is called every frame.
+   * It is supposed to update the game object's state.
+   *
+   * While it *could* modify the global game state,
+   * it is not supposed to do so.
+   */
+  public abstract update(): void;
+}
 
 export default GameObject;
