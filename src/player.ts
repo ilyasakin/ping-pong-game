3a88c5926cd927d7bfb2fccd11fd850204fa594e
İlyas Akın <ashnwor@gmail.com>
2023-02-22 21:04:26 +0300

    Let player(s) control player with controls

--- src/player.ts
+++ src/player.ts
@@ -6,6 +6,21 @@ import PlayerControls from "./player-controls";
 class Player extends Vector2D {
   constructor(private readonly game: Game, public readonly controls: PlayerControls, position: Position) {
     super(position.x, position.y);
+    this.init();
+  }
+
+  private keydownHandler(e: KeyboardEvent): void {
+    if (e.key === this.controls.up) {
+      this.y -= 20;
+    }
+
+    if (e.key === this.controls.down) {
+      this.y += 20;
+    }
+  }
+
+  private init(): void {
+    document.addEventListener('keydown', this.keydownHandler.bind(this));
   }
 
   public draw(ctx: CanvasRenderingContext2D): void {
