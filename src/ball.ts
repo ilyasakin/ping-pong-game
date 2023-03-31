1deb8ab2863d99e2d3ced5598c9ece889a99c6b2
İlyas Akın <ashnwor@gmail.com>
2023-03-31 04:10:29 +0300

    Fix ball rendering

--- src/ball.ts
+++ src/ball.ts
@@ -1,5 +1,7 @@
 import Vector2D from './vector2d';
 import Game from './game';
+import Dimensions2D from './dimensions2d';
+import Position from './position';
 
 class Ball extends Vector2D {
   private game: Game;
@@ -7,13 +9,13 @@ class Ball extends Vector2D {
   constructor(game: Game) {
     const centerX: number = game.canvas.width / 2;
     const centerY: number = game.canvas.height / 2;
-    super(centerX, centerY);
+    super(new Position(centerX, centerY), new Dimensions2D(20, 20));
     this.game = game;
   }
 
   public draw(ctx: CanvasRenderingContext2D): void {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 20, 0, 2 * Math.PI);
+    ctx.arc(this.position.x, this.position.y, 20, 0, 2 * Math.PI);
     ctx.fillStyle = '#FFFFFF';
     ctx.fill();
     ctx.closePath();
