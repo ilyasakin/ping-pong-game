5695d89a9b7d19b70da78edab25fcfd5dfc84a55
İlyas Akın <ashnwor@gmail.com>
2023-03-31 04:05:36 +0300

    Create dimensions2d class

---
+++ src/dimensions2d.ts
@@ -0,0 +1,12 @@
+class Dimensions2D {
+  public width: number;
+
+  public height: number;
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+export default Dimensions2D;
